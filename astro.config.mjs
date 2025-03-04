80ae1139f9645d8829e86ca20f5cdb5afa70ea40
Prozilla <business@prozilla.dev>
2025-03-04 20:12:30 +0100

    Updated astro config + updated dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://prozilla.github.io/Pine-site/",
+	site: "https://prozilla.github.io",
 	integrations: [
 		starlight({
 			title: "Pine",
