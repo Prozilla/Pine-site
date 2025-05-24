79e1c67f5112080507d74da29d435ef3c745db99
Prozilla <business@prozilla.dev>
2025-05-24 14:01:41 +0200

    Fixed home page OG image

--- src/routeData.ts
+++ src/routeData.ts
@@ -2,7 +2,7 @@ import { defineRouteMiddleware } from "@astrojs/starlight/route-data";
 
 export const onRequest = defineRouteMiddleware((context) => {
 	const ogImageUrl = new URL(
-		`/og/${context.locals.starlightRoute.id ?? "index"}.png`,
+		`/og/${context.locals.starlightRoute.id || "index"}.png`,
 		context.site,
 	);
 
